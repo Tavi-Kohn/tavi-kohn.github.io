41ca9a458d88df2a6756b9e0e4dc7c1901e4fbc7
Tavi-Kohn <tavi.kohn@gmail.com>
2017-05-11 10:43:21 -0600

    Update footer with new github contact link and add local dependancies

---
+++ js/githubPopoverButton.js
@@ -0,0 +1,5 @@
+var encodedGithubAddress = "aHR0cHM6Ly9naXRodWIuY29tL1RhdmktS29obg==";	//This must not contain a trailing newline
+
+$("#github-popover-button").popover({content: function() {
+	return "<a class=\"btn btn-secondary\"  onmousedown=\"new function() {var popup = window.open('" + atob(encodedGithubAddress) + "');}\" >" + atob(encodedGithubAddress) + "</a>"
+}, html:true, trigger:"focus"});
