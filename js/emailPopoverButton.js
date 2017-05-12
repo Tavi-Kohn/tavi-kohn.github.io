6a6bfb56c972e6cbf3758c5dabef64394aa4293e
Tavi-Kohn <tavi.kohn@gmail.com>
2017-05-12 09:33:51 -0600

    Fix email link popover to hopefully work on mobile

--- js/emailPopoverButton.js
+++ js/emailPopoverButton.js
@@ -2,7 +2,8 @@ var encodedEmailAddress = "dGF2aS5rb2huQGdtYWlsLmNvbQ==";	//This must not contai
 
 $("#email-popover-button").popover({
 	content: function() {
-		return "<a class=\"btn btn-secondary\"  onmousedown=\"new function() {var popup = window.open('mailto:"+ atob(encodedEmailAddress) + "');popup.close();}\" >mailto:" + atob(encodedEmailAddress) + "</a>"
+		// return "<a class=\"btn btn-secondary\" onmousedown=\"new function() {var popup = window.open('mailto:"+ atob(encodedEmailAddress) + "');popup.close();}\" >mailto:" + atob(encodedEmailAddress) + "</a>"
+		return "<a class=\"btn btn-secondary\" href='mailto:"+ atob(encodedEmailAddress) + "'>mailto:" + atob(encodedEmailAddress) + "</a>"
 	},
 	html:true,
 	trigger:"focus",
